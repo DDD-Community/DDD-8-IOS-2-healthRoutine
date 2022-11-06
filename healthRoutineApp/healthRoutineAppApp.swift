f6a75fe3f92b5c534e571e4c20fedc6e558d5c71
YuGeonHui <ghyu_0906@naver.com>
2022-11-06 20:56:24 +0900

    Initial Commit

---
+++ healthRoutineApp/healthRoutineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthRoutineAppApp.swift
+//  healthRoutineApp
+//
+//  Created by geonhui Yu on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct healthRoutineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
