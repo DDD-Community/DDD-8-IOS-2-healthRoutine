a2f8fcca78f924447a81514a49226ca24b387b04
YuJieun <idpresent@naver.com>
2022-12-01 22:59:44 +0900

    add sample launch screen

--- healthRoutineApp/healthRoutineApp.swift
+++ healthRoutineApp/healthRoutineApp.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @main
 struct healthRoutineApp: App {
+    // 스플래스 잠시멈춤 역할 - 권장 x 일단 확인용으로 추가
+    init() {
+        Thread.sleep(forTimeInterval: 3)
+    }
+    
     var body: some Scene {
         WindowGroup {
             
