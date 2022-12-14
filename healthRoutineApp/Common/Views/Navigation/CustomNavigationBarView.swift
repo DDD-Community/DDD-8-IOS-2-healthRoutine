b0d65c4c3d3675aba965d8d3ff7a3f421af882c7
YuGeonHui <ghyu_0906@naver.com>
2022-12-14 18:47:16 +0900

    Custom NavigationView 개발완료

---
+++ healthRoutineApp/Common/Views/Navigation/CustomNavigationBarView.swift
@@ -0,0 +1,72 @@
+//
+//  CustomNavigationBarView.swift
+//  healthRoutineApp
+//
+//  Created by geonhui Yu on 2022/12/14.
+//
+
+import SwiftUI
+
+struct CustomNavigationBarView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    let showBackButton: Bool
+    let title: String?
+    
+    var body: some View {
+        
+        HStack {
+            
+            if showBackButton {
+                backButton
+            }
+            
+            titleSection
+            
+            Spacer()
+            
+            if showBackButton {
+                backButton
+                    .opacity(0)
+            }
+        }
+        .padding()
+        .foregroundColor(.black) // 색상 변경 여기서하면 됨.
+        .font(.headline)
+        .background(Color.clear.ignoresSafeArea(edges: .top))
+    }
+}
+
+struct CustomNavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        VStack {
+            CustomNavigationBarView(showBackButton: true, title: "Text Title")
+            Spacer()
+        }
+    }
+}
+
+extension CustomNavigationBarView {
+    
+    private var backButton: some View {
+        
+        Button {
+            self.presentationMode.wrappedValue.dismiss()
+        } label: {
+            Image(systemName: "chevron.left")
+        }
+    }
+    
+    private var titleSection: some View {
+        
+        VStack(spacing: 4) {
+            
+            if let title = title {
+                Text(title)
+                    .font(.system(size: 20, weight: .bold))
+            }
+        }
+    }
+}
---
+++ healthRoutineApp/Common/Views/Navigation/CustomNavigationView.swift
@@ -0,0 +1,46 @@
+//
+//  CustomNavigationView.swift
+//  healthRoutineApp
+//
+//  Created by geonhui Yu on 2022/12/14.
+//
+
+import SwiftUI
+
+struct CustomNavigationView<Content: View>: View {
+    
+    let content: Content
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        NavigationView {
+            CustomNavigationBarContainerView {
+                content
+            }
+            .navigationBarHidden(true)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct CustomNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        CustomNavigationView {
+            Color.red.ignoresSafeArea()
+        }
+    }
+}
+
+// MARK: - Gesture
+extension UINavigationController {
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        interactivePopGestureRecognizer?.delegate = nil
+    }
+}
