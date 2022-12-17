294da79bf7f3d4188b22d011d56175ecd6a182b6
유지은 <yoojieun@ssg.com>
2022-12-17 12:03:56 +0900

    >> 제목 : 폰트추가 >> 상세내용 :

---
+++ healthRoutineApp/Extension/Font+Ext.swift
@@ -0,0 +1,35 @@
+//
+//  Font+Ext.swift
+//  healthRoutineApp
+//
+//  Created by 유지은(파트너) - 서비스개발담당App개발팀 on 2022/12/16.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    enum Pretendard {
+        case bold
+        case extraBold
+        case medium
+        case regular
+
+        var value: String {
+            switch self {
+            case .bold:
+                return "Pretendard-Bold"
+            case .medium:
+                return "Pretendard-Medium"
+            case .extraBold:
+                return "Pretendard-ExtraBold"
+            case .regular:
+                return "Pretendard-Regular"
+            }
+        }
+    }
+
+    static func pretendard(_ type: Pretendard, size: CGFloat = 13) -> Font {
+        return .custom(type.value, size: size)
+    }
+}
